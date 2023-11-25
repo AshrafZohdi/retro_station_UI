73a187405f4294c2336b57ff9fc718c4331d857c
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2023-11-25 14:13:47 +0100

    Created React App and Added animated background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AnimatedBackground from "./components/animated-background/AnimatedBacground";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnimatedBackground />
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/components/animated-background/AnimatedBacground.jsx
@@ -0,0 +1,15 @@
+// AnimatedBackground.js
+import React from "react";
+import "./AnimatedBackground.css";
+
+const AnimatedBackground = () => {
+  return (
+    <div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default AnimatedBackground;
